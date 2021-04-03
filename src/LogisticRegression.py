08e9293b42d8456949ae8c875c9a5b429aed01c6
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-04-03 05:30:33 -0400

    sklearn

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import numpy as np
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 
 data = pd.read_csv('../Datasets/UCI-heart-disease.csv')
 df = pd.DataFrame(data)
-x = df.drop("target", axis=1).values
-y = df.target.values
-print(x)
-print("---")
-print(y)
+x = df.drop("target", axis=1)
+y = df.target
+# print(x)
+# print("---")
+# print(y)
 
+# df.head
 # print(x.values)
 # print(x.iloc[:1, :].)
-# clf = LogisticRegression(random_state=0).fit(x, y)
-# clf.predict(X[:2, :])
+clf = LogisticRegression(random_state=0, max_iter=500).fit(x, y)
+# print(x.iloc[:1, :])
+predict = clf.predict(x.head(1))
+prob = clf.predict_proba(x.head(1))
+score = clf.score(x,y)
+print(predict)
+print(prob)
+print(score)
