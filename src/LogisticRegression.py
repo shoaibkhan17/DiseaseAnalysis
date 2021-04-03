0a3d5e36fd3ce21d7e1b5f47c9a188c7229d6454
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-04-03 04:37:29 -0400

    test

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -4,9 +4,13 @@ import numpy as np
 
 data = pd.read_csv('../Datasets/UCI-heart-disease.csv')
 df = pd.DataFrame(data)
-x = df.drop("target", axis=1)
-y = df.target
+x = df.drop("target", axis=1).values
+y = df.target.values
 print(x)
-print(x.iloc[:1, :])
+print("---")
+print(y)
+
+# print(x.values)
+# print(x.iloc[:1, :].)
 # clf = LogisticRegression(random_state=0).fit(x, y)
 # clf.predict(X[:2, :])
