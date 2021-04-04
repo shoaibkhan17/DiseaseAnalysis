7c759a0bfd34c34964a35afffcc72dc8d1783749
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-04-04 03:00:22 -0400

    Creating LogisticRegression class

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -2,22 +2,25 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-data = pd.read_csv('../Datasets/UCI-heart-disease.csv')
-df = pd.DataFrame(data)
-x = df.drop("target", axis=1)
-y = df.target
-# print(x)
-# print("---")
-# print(y)
 
+class LogisticRegression:
+    def __init__(self):
+        self.data = pd.read_csv('../Datasets/heart.csv')
+        self.df = pd.DataFrame(self.data)
+        x = self.df.drop("target", axis=1)
+        y = self.df.target
+        self.clf = LogisticRegression(random_state=0, max_iter=500).fit(x, y)
+
+
+# [63, 1, 3, 145, 233, 1, 0, 150, 0]
 # df.head
 # print(x.values)
 # print(x.iloc[:1, :].)
-clf = LogisticRegression(random_state=0, max_iter=500).fit(x, y)
+# clf = LogisticRegression(random_state=0, max_iter=500).fit(x, y)
 # print(x.iloc[:1, :])
 predict = clf.predict(x.head(1))
 prob = clf.predict_proba(x.head(1))
-score = clf.score(x,y)
+score = clf.score(x, y)
 print(predict)
 print(prob)
 print(score)
