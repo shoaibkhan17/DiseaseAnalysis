34dad3d9d82afc7f413dab2b0bc8d302ab515db5
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-04-04 03:39:52 -0400

    added more CVD parameters + build file

--- frontend/src/components/HeartDisease.js
+++ frontend/src/components/HeartDisease.js
@@ -5,7 +5,6 @@ import {
   LinearProgress,
   Typography,
   FormControl,
-  FormHelperText,
   Select,
   Grid,
 } from "@material-ui/core";
@@ -14,187 +13,256 @@ import "../styles/styles.css";
 import { MenuItem } from "@material-ui/core";
 import { InputLabel } from "@material-ui/core";
 
-const START_TRANSMISSION_URL = "http://localhost:5000/predictHeartDisease";
+const HEART_DISEASE_PREDICTION_URL =
+  "http://localhost:5000/predictHeartDisease";
 
 class HeartDisease extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       age: 63,
-      gender: 1,
+      sex: 1,
       bloodPressure: 145,
       chestPainType: 3,
       serumCholesterol: 233,
       maxHeartRate: 150,
       exerciseAngina: 0,
+      bloodSugar: 1,
+      vesselsColored: 0,
     };
     this.handleClick = this.handleClick.bind(this);
-    this.startTransmission = this.startTransmission.bind(this);
+    this.firstColumn = this.firstColumn.bind(this);
+    this.secondColumn = this.secondColumn.bind(this);
+    this.thirdColumn = this.thirdColumn.bind(this);
+    this.fourthColumn = this.fourthColumn.bind(this);
   }
 
   componentDidMount() {}
 
-  handleClick() {
-    if (!this.state.transmitting) {
-      this.startTransmission();
-    } else {
-      this.setState({ transmitting: false });
-    }
-  }
-
-  async startTransmission() {
+  async handleClick() {
     var obj = {
       age: this.state.age,
-      cFreg: this.state.cFreq,
-      sampleRate: this.state.sampleRate,
-      selectedFile: this.state.selectedFile,
+      sex: this.state.sex,
+      chestPainType: this.state.chestPainType,
+      bloodPressure: this.state.bloodPressure,
+      serumCholesterol: this.state.serumCholesterol,
+      bloodSugar: this.state.bloodSugar,
+      vesselsColored: this.state.vesselsColored,
+      maxHeartRate: this.state.maxHeartRate,
+      exerciseAngina: this.state.exerciseAngina,
     };
 
     axios
-      .post(START_TRANSMISSION_URL, obj)
-      .then((res) => this.setState({ transmitting: true }))
-      .catch((error) => this.console.log(error));
+      .post(HEART_DISEASE_PREDICTION_URL, obj)
+      .then((res) => console.log("sent response"))
+      .catch((error) => console.log(error));
+  }
+
+  firstColumn() {
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <TextField
+            label="Age"
+            placeholder="20"
+            variant="outlined"
+            fullWidth
+            type="number"
+            value={this.state.age}
+            onChange={(event) => this.setState({ age: event.target.value })}
+            style={{ paddingBottom: "4%" }}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel>Sex</InputLabel>
+            <Select
+              label="Sex"
+              value={this.state.sex}
+              onChange={(event) => this.setState({ sex: event.target.value })}
+            >
+              <MenuItem value={0}>Female</MenuItem>
+              <MenuItem value={1}>Male</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel>Chest Pain Type</InputLabel>
+            <Select
+              label="Chest Pain Type"
+              value={this.state.chestPainType}
+              onChange={(event) =>
+                this.setState({ chestPainType: event.target.value })
+              }
+            >
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  secondColumn() {
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+          <TextField
+            label="Resting Blood Pressure (mm Hg)"
+            placeholder="145"
+            variant="outlined"
+            fullWidth
+            type="number"
+            value={this.state.bloodPressure}
+            onChange={(event) =>
+              this.setState({ bloodPressure: event.target.value })
+            }
+            style={{ paddingBottom: "4%" }}
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel>Exercise Induced Angina</InputLabel>
+            <Select
+              label=">Exercise Induced Angina"
+              value={this.state.exerciseAngina}
+              onChange={(event) =>
+                this.setState({ exerciseAngina: event.target.value })
+              }
+            >
+              <MenuItem value={0}>No</MenuItem>
+              <MenuItem value={1}>Yes</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  thirdColumn() {
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel>
+              Fasting Blood Sugar {"&"} Glucose (120 mg/dl)
+            </InputLabel>
+            <Select
+              label={"Fasting Blood Sugar & Glucose (120 mg/dl)"}
+              value={this.state.bloodSugar}
+              onChange={(event) =>
+                this.setState({ bloodSugar: event.target.value })
+              }
+            >
+              <MenuItem value={0}>No</MenuItem>
+              <MenuItem value={1}>Yes</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            label="Serum Cholesterol (mg/dl)"
+            placeholder="233"
+            variant="outlined"
+            fullWidth
+            type="number"
+            value={this.state.serumCholesterol}
+            onChange={(event) =>
+              this.setState({ serumCholesterol: event.target.value })
+            }
+            style={{ paddingBottom: "4%" }}
+          />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  fourthColumn() {
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel>Major Vessels Colored</InputLabel>
+            <Select
+              label={"Major Vessels Colored"}
+              value={this.state.vesselsColored}
+              onChange={(event) =>
+                this.setState({ vesselsColored: event.target.value })
+              }
+            >
+              <MenuItem value={0}>0</MenuItem>
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            label="Maximum Heart Rate (BPM)"
+            placeholder="150"
+            variant="outlined"
+            fullWidth
+            type="number"
+            value={this.state.maxHeartRate}
+            onChange={(event) =>
+              this.setState({ maxHeartRate: event.target.value })
+            }
+            style={{ paddingBottom: "4%" }}
+          />
+        </Grid>
+      </Grid>
+    );
   }
 
   render() {
     return (
       <div className="box">
-        <div className="title">
-          <Typography
-            variant="h6"
-            style={{
-              paddingBottom: "15px",
-              color: "rgb(50, 152, 220)",
-              textDecoration: "underline",
-            }}
-          >
-            Enter Cardiovascular Parameters
-          </Typography>
-        </div>
-        <div className="formContainer">
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                label="Age"
-                placeholder="20"
-                variant="outlined"
-                fullWidth
-                type="number"
-                value={this.state.age}
-                onChange={(event) => this.setState({ age: event.target.value })}
-                style={{ paddingBottom: "4%" }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <FormControl variant="outlined" fullWidth>
-                <InputLabel>Gender</InputLabel>
-                <Select
-                  label="Gender"
-                  value={this.state.gender}
-                  onChange={(event) =>
-                    this.setState({ gender: event.target.value })
-                  }
-                >
-                  <MenuItem value={0}>Female</MenuItem>
-                  <MenuItem value={1}>Male</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          alignItems="stretch"
+          spacing={5}
+        >
+          <Grid item>
+            <Typography
+              variant="h6"
+              style={{
+                paddingBottom: "15px",
+                color: "rgb(50, 152, 220)",
+                textDecoration: "underline",
+              }}
+            >
+              Enter Cardiovascular Parameters
+            </Typography>
           </Grid>
 
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                label="Resting Blood Pressure"
-                placeholder="145"
-                variant="outlined"
-                fullWidth
-                type="number"
-                value={this.state.bloodPressure}
-                onChange={(event) =>
-                  this.setState({ bloodPressure: event.target.value })
-                }
-                style={{ paddingBottom: "4%" }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <FormControl variant="outlined" fullWidth>
-                <InputLabel>Chest Pain Type (mm Hg)</InputLabel>
-                <Select
-                  label="Chest Pain Type (mm Hg)"
-                  value={this.state.chestPainType}
-                  onChange={(event) =>
-                    this.setState({ chestPainType: event.target.value })
-                  }
-                >
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
+          <Grid item>{this.firstColumn()}</Grid>
 
-          <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <TextField
-                label="Serum Cholesterol (mg/dl)"
-                placeholder="233"
-                variant="outlined"
-                fullWidth
-                type="number"
-                value={this.state.serumCholesterol}
-                onChange={(event) =>
-                  this.setState({ serumCholesterol: event.target.value })
-                }
-                style={{ paddingBottom: "4%" }}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                label="Maximum Heart Rate (BPM)"
-                placeholder="150"
-                variant="outlined"
-                fullWidth
-                type="number"
-                value={this.state.maxHeartRate}
-                onChange={(event) =>
-                  this.setState({ maxHeartRate: event.target.value })
-                }
-                style={{ paddingBottom: "4%" }}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <FormControl variant="outlined" fullWidth>
-                <InputLabel>Exercise Induced Angina</InputLabel>
-                <Select
-                  label=">Exercise Induced Angina"
-                  value={this.state.exerciseAngina}
-                  onChange={(event) =>
-                    this.setState({ exerciseAngina: event.target.value })
-                  }
-                >
-                  <MenuItem value={0}>No</MenuItem>
-                  <MenuItem value={1}>Yes</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
-        </div>
+          <Grid item>{this.secondColumn()}</Grid>
 
-        <div className="buttonContainer">
-          <Button
-            variant="contained"
-            fullWidth
-            color={this.state.transmitting ? "secondary" : "primary"}
-            // onClick={this.handleClick}
-          >
-            {this.state.transmitting ? "Stop Transmitting" : "Predict"}
-          </Button>
-          {this.state.transmitting && (
-            <LinearProgress color="primary" style={{ color: "#33eb91" }} />
-          )}
-        </div>
+          <Grid item>{this.thirdColumn()}</Grid>
+
+          <Grid item>{this.fourthColumn()}</Grid>
+
+          <Grid item>
+            <Button
+              variant="contained"
+              fullWidth
+              color={"primary"}
+              onClick={this.handleClick}
+            >
+              Predict
+            </Button>
+            {/* {this.state.predicting && (
+              <LinearProgress color="primary" style={{ color: "#33eb91" }} />
+            )} */}
+          </Grid>
+        </Grid>
       </div>
     );
   }
