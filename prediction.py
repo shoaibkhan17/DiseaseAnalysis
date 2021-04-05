4e00694185b66be0251757a1523d47f5e4b4cf99
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-04-05 05:32:04 -0400

    Cleaned the datasets and updated the prediction
    Prediction now supports
    -Stroke
    -Liver
    
    Cleaned datasets
    -Stroke
    -Liver

--- prediction.py
+++ prediction.py
@@ -4,13 +4,13 @@ from sklearn.linear_model import LogisticRegression
 
 
 class Prediction:
-    def __init__(self, filename):
-        self.data = pd.read_csv('datasets/' + filename)
+    def __init__(self, filename, columnName):
+        self.data = pd.read_csv('cleanedDatasets/' + filename + ".csv")
         self.df = pd.DataFrame(self.data)
-        self.x = self.df.drop("target", axis=1)
-        self.y = self.df.target
+        self.x = self.df.drop(columnName, axis=1)
+        self.y = self.df[columnName]
         self.clf = LogisticRegression(
-            random_state=0, max_iter=700).fit(self.x, self.y)
+            random_state=0, max_iter=1000).fit(self.x, self.y)
 
     def predict(self, lst):
         predict = self.clf.predict(lst)
@@ -23,3 +23,48 @@ class Prediction:
     def score(self):
         score = self.clf.score(self.x, self.y)
         return score
+
+
+# Clean and save heart dataset
+# heartDF = pd.read_csv('datasets/heart.csv')
+# heartColumn = "target"
+# predictionHeart = Prediction(heartDF, heartColumn)
+# heartDF.to_csv('cleanedDatasets/heart.csv', index=False, header=True)
+# print(predictionHeart.score())
+
+# Clean and save stroke dataset
+# strokeDF = pd.read_csv('datasets/stroke.csv')
+# strokeColumn = "stroke"
+# strokeDF.drop(columns=['id'], inplace=True)
+# strokeDF.drop(columns=['ever_married'], inplace=True)
+# strokeDF.drop(columns=['work_type'], inplace=True)
+# strokeDF.drop(columns=['Residence_type'], inplace=True)
+# strokeDF.drop(columns=['smoking_status'], inplace=True)
+# strokeDF[strokeDF['stroke'] == 1 & strokeDF['bmi'].isna()]
+# strokeDF['bmi'].fillna(np.round(strokeDF['bmi'].mean(), 1), inplace=True)
+# strokeDF = strokeDF[strokeDF['gender'] != 'Other']
+# strokeDF['gender'] = strokeDF.gender.replace({'Male': 1, 'Female': 0})
+# predictionStroke = Prediction(strokeDF, strokeColumn)
+# strokeDF.to_csv('cleanedDatasets/stroke.csv', index=False, header=True)
+# print(predictionStroke.score())
+
+# Clean and save liver dataset
+# liverDF = pd.read_csv('datasets/liver.csv')
+# liverColumn = "Liver_Disease"
+# liverDF.drop(columns=['Direct_Bilirubin'], inplace=True)
+# liverDF['Gender'] = liverDF.Gender.replace({'Male': 1, 'Female': 0})
+# liverDF.rename(columns={'Dataset': 'Liver_Disease'}, inplace=True)
+# liverDF = liverDF.fillna(liverDF.mean())
+# liverDF.to_csv('cleanedDatasets/liver.csv', index=False, header=True)
+# predictionLiver = Prediction(liverDF, liverColumn)
+# print(predictionLiver.score())
+
+# Prediction scores
+# heart = Prediction("heart", "target")
+# print(heart.score())
+
+# stroke = Prediction("stroke", "stroke")
+# print(stroke.score())
+
+# liver = Prediction("liver", "Liver_Disease")
+# print(liver.score())
